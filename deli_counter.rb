d15764edfeeceb2a7060ebebabf56ecd80632787
David Markel <davidgmarkel@gmail.com>
2018-03-12 01:35:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,10 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     while deli_counter < katz_deli.length
-      prints "The line is currently: #{counter + 1}.: katz_deli[deli_counter]
+      print "The line is currently: #{counter + 1}.: katz_deli[deli_counter]
       counter += 1
     end
+
   end
+
 end
