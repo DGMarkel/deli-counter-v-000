703d056a52c71a0b357ee723f7709d1feff5e1e3
David Markel <davidgmarkel@gmail.com>
2018-03-12 01:33:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,7 @@
 def line(katz_deli)
+
   deli_counter = 0
+
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
@@ -9,4 +11,4 @@ def line(katz_deli)
     end
   end
 end
-end
+
