0bdf4b239286249e06f15babcfa101687c960eed
David Markel <davidgmarkel@gmail.com>
2018-03-12 02:00:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    deli_line = ["The line is currently: "]
+    deli_line = ["The line is currently:"]
     katz_deli.each{|customer|
       customer = "#{katz_deli.index(customer) + 1}. #{customer}"
       deli_line.push(customer)}
