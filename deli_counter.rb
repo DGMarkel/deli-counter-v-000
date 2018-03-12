3b88906c127a70f2e8c5d229defaf0751a3b5fdf
David Markel <davidgmarkel@gmail.com>
2018-03-12 01:26:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,7 @@
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
+  else
+    puts "The line is currently: #{katz_deli.join("#{katz_deli.index + 1}")}"
   end
 end
