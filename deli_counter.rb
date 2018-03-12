5548e05eef509d1cf2cae8c3a7ec978fdebf2c75
David Markel <davidgmarkel@gmail.com>
2018-03-12 02:04:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,8 @@ def line(katz_deli)
     puts deli_line.join(" ")
   end
 end
+
+def take_a_number(katz_deli, new_customer)
+  katz_deli << new_customer
+  puts "Welcome, #{new_customer}.  Your are number #{katz_deli.index(new_customer) + 1} in line."
+end
