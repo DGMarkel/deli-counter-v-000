70e8f63e9d40ef523417608bc38f4f8208d5ac9d
David Markel <davidgmarkel@gmail.com>
2018-03-12 01:43:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   else
     print "The line is currently: "
     while deli_counter < katz_deli.length
- 
+
       print "#{deli_counter + 1}. #{katz_deli[deli_counter]} "
       deli_counter += 1
     end
